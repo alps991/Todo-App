a30eaea81b94342bbc3176dd4381896f6f4cf1b4
Alper Donmez <alper.donmez490@gmail.com>
2018-10-08 00:15:19 -0400

    updated

--- server/db/config.js
+++ server/db/config.js
@@ -1,9 +1,9 @@
-const env = process.env.NODE_ENV || "development";
+var env = process.env.NODE_ENV || 'development';
 
-if (env == "development") {
+if (env === 'development') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URL = 'mongodb://localhost:27017/Todo';
-} else if (env == "test") {
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URL = 'mongodb://localhost:27017/TodoTest';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
