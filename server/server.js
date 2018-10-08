debb084a7fd6dadfe926583f86f5ecbbc92c4e60
Alper Donmez <alper.donmez490@gmail.com>
2018-10-07 23:51:58 -0400

    added delete and patch endpoints

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/user');
@@ -57,7 +58,24 @@ app.delete('/todos/:id', (req, res) => {
     }, err => {
         res.status(404).send();
     })
-})
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then(doc => {
+        res.send({ doc });
+    }, err => res.status(404).send());
+});
 
 app.listen(3000, () => {
     console.log("server started on port 3000");
